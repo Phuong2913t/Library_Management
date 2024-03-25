e003ce3c34eda794830dcf89e5e398ef04952ae9
Phuong2913t <nguyenthuphuong2913@gmail.com>
2024-03-25 20:36:11 +0700

    Issue book backend

--- IssueBook.cs
+++ IssueBook.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,64 @@ namespace LibraryManagement
         public IssueBook()
         {
             InitializeComponent();
+        }
+
+        private void IssueBook_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "data source = DESKTOP-QM47I1B\\SQLEXPRESS01 ; database = LibraryManagement ; integrated security=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+
+            cmd = new SqlCommand("select bName from NewBook", con);
+            SqlDataReader Sdr = cmd.ExecuteReader();
+
+            while (Sdr.Read())
+            {
+                for(int i=0; i<Sdr.FieldCount; i++)
+                {
+                    cboBname.Items.Add(Sdr.GetString(i));
+                }
+                
+            }
+            Sdr.Close();
+            con.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(txtSearchStudent.Text != "")
+            {
+                String eid = txtSearchStudent.Text;
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "data source = DESKTOP-QM47I1B\\SQLEXPRESS01 ; database = LibraryManagement ; integrated security=True";
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = "select * from NewStudent where enroll = '" + eid + "'";
+                SqlDataAdapter DA = new SqlDataAdapter(cmd);
+                DataSet DS = new DataSet();
+                DA.Fill(DS);
+
+                if(DS.Tables[0].Rows.Count != 0)
+                {
+                    txtSname.Text = DS.Tables[0].Rows[0][1].ToString();
+                    txtDep.Text = DS.Tables[0].Rows[0][3].ToString();
+                    txtSem.Text = DS.Tables[0].Rows[0][4].ToString();
+                    txtContact.Text = DS.Tables[0].Rows[0][5].ToString();
+                    txtEmail.Text = DS.Tables[0].Rows[0][6].ToString();
+                }
+                else
+                {
+                    txtSname.Clear();
+                    txtDep.Clear();
+                    txtSem.Clear();
+                    txtContact.Clear();
+                    txtEmail.Clear();
+                    MessageBox.Show("Invalid Enrollement No", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
